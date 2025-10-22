5d9ebb15d95761552881b1cd2550cd8fdf3a46bb
viviennetian <139000714+viviennetian@users.noreply.github.com>
2025-10-21 22:24:01 -0400

    251021_assignment4_cakeFactory3

--- assignment4/README.md
+++ assignment4/README.md
@@ -1,61 +1,138 @@
-# template
+# Assignment 4 : Cake Factory
+
+## Object Factory, Week 6-7 Assignment
 
 ---
 
-This is a README file that can be used to describe and document your assignment.
+## Concept
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+Basically, I want to draw cakes. Because cake is same as pizza with round shape and have a lot of variable decoration on the top. I searched for some vintage style reference pictures on Pinterest for color inspiration.
 
----
+![reference1 pic](./reference1.jpg)
+[link of reference1](https://www.pinterest.com/pin/10766486606331845/)
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
+![reference2 pic](./reference2.jpg)
+[link of reference2](https://www.pinterest.com/pin/3166662232308631/)
 
-**bold text**
+But I didn't fully draw my cakes as same as these, these are to fancy for the assignment. Just get a vintage cake vibe.
 
-*italicized text*
+## Progress
 
-~~strikethrough text~~
+### Circle to Ellipse
 
-Ordered List:
-1. First item
-2. Second item
-3. Third item
+Because cakes are usually drawn with a slight top-down angle, the circular top looks like an ellipse.
+Start with Change Circle Path of Polar Coordinates to Ellipse Path.
+For a circle, the parametric equation describes a point moving around the center at an equal distance in every direction.
+For an ellipse, it’s almost the same idea, but the distance in the horizontal and vertical directions is different. Instead of one radius r, we have two: a for the x-axis and b for the y-axis.
+So to get the polar coordinates of a ellipse, we only change the scale, the x value follows the width, and the y value follows the height.
 
-Unordered List:
-- First item
-- Second item
-- Third item
+With this mathematical formula, I tried to draw a cake with a plate in `draw()` function.
 
-`short code block`
+### Put parameters and drawing function into cake class.
+
+I create a cake class.
+And put variables into parameters. Put everything inside draw function into a method. And separate them to `drawPlate()` and `drawCakeBase()`
+
+### Draw the Cake Base with Two Ellipse and a Rectangle
+
+1. random size
+   Making variable in the ellipse and rectangle relevant to each other, base on ellipse's width.
+
+2. random color
+   I set the colorMode to HSB in `setup();`, because I need a lot of changing color HUE with same saturation and brightness.
+   for example for the cake base:
+   `fill(this.baseHue, 30, 100);`
+   and top layer with a lighter color:
+   `fill(this.baseHue, 20, 100);`
+
+   and when creating a new object, I pass a random hue value `random(0, 360)` pass a HUE value to instance property `this.baseHue`.
+
+### Draw the Plate
+
+1. random color, connected with cake base
+   Plate also have color connection with cake base. But I slightly changed it with `fill(this.baseHue * 3, 30, 60);`
+
+### push object in to array, with `mousePressed()`
+
+I defined a global variable called cakes at the top of the code. It’s an empty array that stores all the cake objects.
+inside the mousePressed(), I create new cakes objects, and push them into array.
 
 ```
-extended code block
-fun() {
-  return 0
+let tempCake = new Cake(
+//===== cake base =====
+mouseX, // x pos of Ellipse1, cake plate center
+mouseY, //y pos of Ellipse1, cake plate center
+
+      random(0, 360), //color of base cake
+    );
+    //take my newly created cake and add it to my cakes array
+    cakes.push(tempCake);
+```
+
+### Draw the Cream on the top
+
+Go back to the class get more decorations for the cake. I decide that every cake in my factory come with cream top and a colorful cream around the bottom. And creams are just like number texts in clock assignment.
+
+1. Polar coordinates with ellipse. For each small cream piece, the position is calculated using the ellipse’s parametric idea: start from the cake center, move horizontally by width × cosine(angle) and vertically by height × sine(angle). By looping through multiple angles, the points are evenly distributed along the elliptical path.
+
+2. I drew two sets of cream loops: One on the top surface. One around the bottom half of the cake.
+
+### Draw Three Type of Decorations
+
+I define three methods in side the class for cake decoration.“to prepare for the type-checking logic used when creating different cakes.
+
+1.`mango()` for mango pieces.
+How to put mango pieces in the cake top area?
+I use `mangoPositions = []` array in class constructor. And a for loop and a polar coordinates with random inside it.
+
+2. `lemon()` to lemon on the top of the cream.
+   Most as same as cream. Add some offset on the y position. And change ellipse to arc.
+
+3. `HEDText()` for Happy EveryDay text with a candle on it.
+   I want to draw a animated fire. And it looks like drunk walk example, so I learned from that, add some random value to the fire position
+   I want the candle size dependent on the cake size, so I use `hEllipse` as a reference.
+
+What special is that I put a animated fire on the top of candle.
+I feel fire animation really looks like drunk walk example, so I learned from that, add some random value to the fire position X and Y.
+
+### Type Changing with `keyPressed()`
+
+Also using template on the class.
+When a key is pressed, the keyPressed() function updates the global variable decoType, which controls what kind of decoration each new cake will have.
+
+1. At the top of the sketch, a global variable named decoType is initialized as "cream".
+   This variable stores the current decoration mode. Every time a new cake is created (in mousePressed()), this variable determines what type of topping will be drawn.
+
+2. The keyPressed() function listens for keys and updates the decoration type:
+
+```
+function keyPressed() {
+  if (key === "m") {
+    decoType = "mango";
+  } else if (key === "l") {
+    decoType = "lemon";
+  } else if (key === "h") {
+    decoType = "HEDText";
+  } else if (key === "c") {
+    decoType = "cream";
+  }
 }
 ```
 
-Link:  
-[linked text](https://www.example.com)
+3. When the user clicks to create a new cake, the current `decoType` value is passed into the cake class constructor. Inside the `draw()` loop, the code checks each cake’s `decoType` and calls the corresponding drawing function (`mango()`, `lemon()`, `HEDText()`, or just `cream()`).
 
+### click on the top of cake to delete the cake
 
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
+Also using template on the class.
 
+1. Add a `this.hovering`instance property and a `checkHover()` method in the cake class, using `let d = dist(mouseX, mouseY, this.xEllipse, this.yEllipseTop);`to detect the distance between mouse position and the center of cake top surface.
 
-Image on repo:  
-![image description](./file-name.jpg)
+2. This check runs every frame inside the draw() loop, so each cake continuously knows whether it’s being hovered over.
 
+array.splice and hovering detection.
 
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
+3. When the mouse is pressed, the program loops through all cakes to check which one is currently being hovered. If hovering is true for any cake, It removes that cake from the global cakes array using `cakes.splice(i, 1)`. The splice method deletes one element (the clicked cake) at index i.
 
+4. If no cake is hovered (amIHovering == false), a new cake is created at the mouse position instead.
 
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
+5. I also put the background color and text prompts inside the draw() function, so they are constantly refreshed without being affected.
--- assignment4/sketch.js
+++ assignment4/sketch.js
@@ -1,4 +1,6 @@
-let cake1;
+let cakes = [];
+let decoType = "cream";
+let numberOfMango = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,134 +8,236 @@ function setup() {
   //H: Hue, S: saturation, B: brightness.
   // colorMode(HSB, maxH, maxS, maxB, maxA);
   //  https://p5js.org/reference/p5/colorMode/
-  background(160, 20, 200);
-
-  cake1 = new Cake(
-    //cake base
-    width / 2, //center x
-    height / 2, //y pos of Ellipse1
-    height / 2 + 80, //y pos of Ellipse2
-    200, //width of ellipses and rect
-    140, //height of ellipses
-    height / 2 + 40, //y pos of rect
-    80, //height of rect
-    0, //color of base cake
-    TWO_PI / 18
-  );
 }
 
 function draw() {
-  cake1.drawPlate();
-  cake1.drawCakeBase();
-  if (cake1.decoType === "cream") {
-    cake1.cream();
+  background(40, 40, 30);
+  textSize(16);
+  stroke(10, 90, 100);
+  fill(100);
+  text("welcome to cake factory!", 20, 20);
+  text("press 'm' order for mango cake", 20, 40);
+  text("press 'l' order for lemon cake", 20, 60);
+  text("press 'h' order for 'happy everyday' cake", 20, 80);
+  text("press 'c' order for regular cream cake", 20, 100);
+  text("click on top to cancel the order", 20, 120);
+
+  //draw all cakes in the cakes array
+  for (let i = 0; i < cakes.length; i++) {
+    cakes[i].checkHover(); //check if hovering
+
+    cakes[i].drawPlate();
+    cakes[i].drawCakeBase();
+    cakes[i].cream(); //all cakes have cream
+
+    //based on decoration type, draw different decorations
+    if (cakes[i].decoType === "mango") {
+      cakes[i].mango();
+    } else if (cakes[i].decoType === "lemon") {
+      cakes[i].lemon();
+    } else if (cakes[i].decoType === "HEDText") {
+      cakes[i].HEDText();
+    }
   }
-  cake1.mango();
 }
 
 function mousePressed() {
-  cake1.decoType = "cream";
-  cake1.creamType = "circle";
+  let amIHovering = false;
+
+  //===== hovering and click to delete the cake from array =====
+  for (let i = 0; i < cakes.length; i++) {
+    if (cakes[i].hovering == true) {
+      //Array.splice is a method for removing elements form an array
+      //parameter one is the element to remove
+      //parameter two is how many elements to remove
+      cakes.splice(i, 1); //erase element i form our array, erase only one element
+
+      amIHovering = true;
+    }
+  }
+
+  //if not clicking on any cake, create a new cake at mouse position
+  if (amIHovering == false) {
+    //if no hovering was detected in previous for loop
+
+    let tempCake = new Cake(
+      //===== cake base =====
+      mouseX, // x pos of Ellipse1, cake plate center
+      mouseY, //y pos of Ellipse1, cake plate center
+
+      random(0, 360), //color of base cake
+
+      //===== cream =====
+      TWO_PI / 18,
+      TWO_PI / 24,
+      //===== top decorations =====
+      decoType,
+      numberOfMango //number of mango
+    );
+    //take my newly created cake and add it to my cakes array
+    cakes.push(tempCake);
+  }
+}
+
+function keyPressed() {
+  if (key === "m") {
+    decoType = "mango";
+  } else if (key === "l") {
+    decoType = "lemon";
+  } else if (key === "h") {
+    decoType = "HEDText";
+  } else if (key === "c") {
+    decoType = "cream";
+  }
 }
 
 class Cake {
   // class declares a new type of object
   constructor(
-    x,
-    yEllipse1,
-    yEllipse2,
-    widthE,
-    heightE,
-    yRect,
-    heightRect,
-    hue,
-    angle
+    x, // x pos of ellipseTop, plate center
+    y, //y pos of ellipseTop, plate center
+
+    hue, //color of base cake
+
+    creamAngle, //cream angle
+    creamBottomAngle, //cream bottom angle
+
+    decoType,
+    numberOfMango //number of mango
   ) {
+    //for deleting cake when hovering and clicking
+    this.hovering = false;
+
+    //=== plate parameters ===
+    this.plateDiameterX = random(200, 450);
+    this.plateDiameterY = this.plateDiameterX * 0.7;
+
     //=== Cake Base parameters ===
+    //I used two ellipses and one rectangle to create a cake base
+
+    //one rectangle, rect(x, y, w, [h])，
+    //rect is the height of the cake
+
+    //plate size decides cake size
+    this.wRect = this.plateDiameterX / 2;
+    this.hRect = random(80, 120);
+
+    //center position of rectangle
+    this.xRect = x;
+    this.yRect = y + this.hRect / 2;
+
     //two ellipse, ellipse(x, y, w, [h])
-    this.xE = x;
-    this.yE1 = yEllipse1;
-    this.yE2 = yEllipse2;
-    this.wEllipse = widthE;
-    this.hEllipse = heightE;
-    //one rectangle, rect(x, y, w, [h])
-    this.xR = x;
-    this.yR = yRect;
-    this.wR = widthE;
-    this.hR = heightRect;
+    this.xEllipse = x; //center x position for both ellipses
+
+    this.yEllipseTop = y; //cake top surface position y
+    this.yEllipseBottom = this.yEllipseTop + this.hRect; //cake bottom surface position y
+    //the size of the cake will be based on the plate size
+    this.wEllipse = this.plateDiameterX / 2;
+    this.hEllipse = this.plateDiameterY / 2;
+
+    //=== base cake color ===
     this.baseHue = hue;
 
-    //all types of deco have cream,cream have different shapes
-    this.creamAngle = angle;
-    this.creamD = 40;
-    this.creamType = "circle";
+    //all types of deco have cream
+    this.creamAngle = creamAngle;
+    this.creamBottomAngle = creamBottomAngle;
+    this.creamD = this.wEllipse / 5;
 
     //=== top decorations ===
-    this.decoType = "mango";
-    this.numMango = 80;
-
-    //  this.decoType = "cherries";
-    //  this.decoType = "HBD text";
-    //  this.decoType = "mango";
-    //  this.decoType = "lemon";
-    //deco Type Also can be:"cherries","HBD text","mango"
+    this.decoType = decoType;
+    this.numberOfMango = numberOfMango;
+    this.mangoPositions = [];
+    //position of mango, mango position X and mango position Y, randomly generated within the cake top area
+    //used example template from recorded course video
+    for (let i = 0; i < this.numberOfMango; i++) {
+      let spacing = 360 / this.numberOfMango;
+      let mangoX =
+        cos(radians(i * spacing)) * random(10, this.wEllipse / 2 - 20);
+      let mangoY =
+        sin(radians(i * spacing)) * random(10, this.hEllipse / 2 - 20);
+      let newVector = createVector(mangoX, mangoY);
+      this.mangoPositions.push(newVector);
+    }
+    this.mangoAngles = [];
+    for (let i = 0; i < this.numberOfMango; i++) {
+      this.mangoAngles.push(random(-0.5, 0.5));
+    }
   }
 
-  //===== I can randomly change the plate style ?
+  //draw the plate
   drawPlate() {
-    noStroke();
+    stroke(1);
     //plate
-    fill(250, 200, 30);
-    ellipse(this.xE, this.yE2 + 8, 400, 280);
-    fill(0, 200, 30);
-    ellipse(this.xE, this.yE2, 400, 280);
+    fill(this.baseHue, 20, 30);
+    ellipse(
+      this.xEllipse,
+      this.yEllipseBottom + 8,
+      this.plateDiameterX,
+      this.plateDiameterY
+    );
+    fill(this.baseHue * 3, 30, 60);
+    ellipse(
+      this.xEllipse,
+      this.yEllipseBottom,
+      this.plateDiameterX,
+      this.plateDiameterY
+    );
   }
 
+  //draw the cake base, the size based on plate size
   drawCakeBase() {
     noStroke();
     //Base Cake
-    fill(this.baseHue, 60, 255);
-    ellipse(this.xE, this.yE2, this.wEllipse, this.hEllipse);
+    fill(this.baseHue, 30, 100);
+    ellipse(this.xEllipse, this.yEllipseBottom, this.wEllipse, this.hEllipse);
     //I want to base on the center position, to draw the rectangle, so that ellipse and rect can use the same x center position.
     //https://p5js.org/reference/p5/rectMode/
     rectMode(CENTER);
-    rect(this.xR, this.yR, this.wR, this.hR);
+    rect(this.xRect, this.yRect, this.wRect, this.hRect);
     //top layer lighter color
-    fill(this.baseHue, 40, 255);
-    ellipse(this.xE, this.yE1, this.wEllipse, this.hEllipse);
+    fill(this.baseHue, 20, 100);
+    ellipse(this.xEllipse, this.yEllipseTop, this.wEllipse, this.hEllipse);
   }
 
+  //use polar coordinates to draw cream around the cake top
   cream() {
-    noStroke();
-    fill(0, 0, 255);
+    stroke(10, 90, 100);
+    fill(0, 0, 100);
     for (let i = 0; i < 18; i++) {
       let currentAngle = this.creamAngle * i;
       //this is the parametric equations of an ellipse
-      //You start from the center point (cx, cy), then move horizontally by "a * cos(theta) and vertically by b * sin(theta). As theta goes from 0 to 360 degrees, the point traces out the shape of an ellipse.
-      let x = this.xE + cos(currentAngle) * (this.wEllipse / 2.2);
-      let y = this.yE1 + sin(currentAngle) * (this.hEllipse / 2.2);
+      //start from the center point (cx, cy), then move horizontally by "a * cos(theta) and vertically by b * sin(theta). As theta goes from 0 to 360 degrees, the point traces out the shape of an ellipse
+      let x = this.xEllipse + cos(currentAngle) * (this.wEllipse / 2.2);
+      let y = this.yEllipseTop + sin(currentAngle) * (this.hEllipse / 2.2);
 
-      // ===== I can create more cream type =====?
-      if (this.creamType === "circle") {
-        circle(x, y, this.creamD);
-      }
-      if (this.creamType === "semiCircle") {
-        arc(x, y, this.creamD, this.creamD, PI, TWO_PI, PIE);
-      }
+      ellipse(x, y, this.creamD / 1.2, this.creamD / 1.4);
+    }
+
+    //bottom colorful cream
+    //choose only to draw half of the bottom cream, so i starts from 2 to 11
+    for (let i = 2; i < 11; i++) {
+      let currentAngle = this.creamBottomAngle * i;
+      let x = this.xEllipse + cos(currentAngle) * (this.wEllipse / 2);
+      let y = this.yEllipseBottom - 5 + sin(currentAngle) * (this.hEllipse / 2);
+      fill(this.baseHue, 15, 100);
+      ellipse(x, y, this.creamD / 1.2, this.creamD / 1.4);
     }
   }
 
   lemon() {
-    noStroke();
-    fill(50, 255, 255);
+    push(); //I need to add push and pop, or it will affect text on the left top corner
+    stroke(40, 90, 100);
+    fill(50, 100, 100);
     for (let i = 0; i < 18; i++) {
       let currentAngle = this.creamAngle * i;
-      let x = this.xE + cos(currentAngle) * (this.wEllipse / 2.2) * sqrt(r);
-      let y = this.yE1 + sin(currentAngle) * (this.hEllipse / 2.2) * sqrt(r);
-      fill(50, 255, 255);
+      let x = this.xEllipse + cos(currentAngle) * (this.wEllipse / 2.2);
+      let y = this.yEllipseTop + sin(currentAngle) * (this.hEllipse / 2.2);
+      fill(50, 100, 100);
       //draw the pericarp
+      //same as cream, but use arc to draw part of the ellipse
       arc(x, y - 10, this.creamD, this.creamD, PI / 6, PI + PI / 6, PIE);
-      fill(50, 10, 255);
+      //use a smaller arc to draw the pulp, and some offset
+      fill(50, 10, 100);
       arc(
         x + 2,
         y - 10 - 2,
@@ -144,20 +248,66 @@ class Cake {
         PIE
       );
     }
+    pop();
   }
+
   mango() {
+    //to draw shapes inside certain area
+    //define the position of mango based on pre-calculated positions
+    //used example template from recorded course video
+    for (let i = 0; i < this.numberOfMango; i++) {
+      push();
+      rectMode(CENTER);
+      stroke(40, 90, 100);
+      fill(50, 50, 100);
+      //move the canvas origin to the mango position
+      translate(
+        this.mangoPositions[i].x + this.xEllipse,
+        this.mangoPositions[i].y + this.yEllipseTop
+      );
+      rotate(this.mangoAngles[i]);
+      rect(0, 0, 20, 20);
+
+      pop();
+    }
+  }
+
+  HEDText() {
+    //candle
+    //also I want to draw a animated fire. And it looks like drunk walk example, so I learned from that, add some random value to the fire position
+    //I want the candle size dependent on the cake size, so I use hEllipse as a reference
+
+    push(); //I need to add push and pop, or it will affect text on the left top corner
     rectMode(CENTER);
+    fill(100);
+    rect(
+      this.xEllipse,
+      this.yEllipseTop - this.hEllipse * 0.5,
+      this.hEllipse * 0.1,
+      this.hEllipse * 0.6
+    );
+    //draw the fire
+    let fireX = this.xEllipse + random(-2, 2);
+    let fireY = this.yEllipseTop - this.hEllipse * 0.9 + random(-2, 2);
+    fill(30, 100, 100);
+    ellipse(fireX, fireY, this.hEllipse * 0.1, this.hEllipse * 0.2);
+
+    //Text
     noStroke();
-    fill(50, 255, 255);
-
-    //how to draw shapes inside certain area??
+    fill(0, 100, 100);
+    textAlign(CENTER, CENTER);
+    textSize(10 * (this.wEllipse / 100));
+    text("HAPPY", this.xEllipse, this.yEllipseTop);
+    text(
+      "EVERYDAY",
+      this.xEllipse,
+      this.yEllipseTop + 10 * (this.wEllipse / 100)
+    );
+    pop();
   }
 
-  HBDText() {
-    text("happy");
-    text("birthday");
+  checkHover() {
+    let d = dist(mouseX, mouseY, this.xEllipse, this.yEllipseTop);
+    this.hovering = d < this.wEllipse / 2; // check if mouse is within the cake top area
   }
-
-  //outside of cake decorations
-  sparkling() {}
 }
